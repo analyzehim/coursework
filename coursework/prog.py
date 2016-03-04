71c94ec69f7f76ed618a9d74dbb1c984f6b49492
L4rs_ubu <so@so.so>
2016-03-04 11:19:46 +0300

    Create F_beta without minimization

--- coursework/prog.py
+++ coursework/prog.py
@@ -1,31 +1,53 @@
-import random
+import numpy as np
 import time
+
+m = [0.0101110, 0.0043532, 0.0137058]
+V = [[0.00324625, 0.00022983, 0.00420395],
+     [0.00022983, 0.00049937, 0.00019247],
+     [0.00420395, 0.00019247, 0.00764097]]
 q = 1000
 beta = 0.9
-def generate_sample():
-        while True:
-                y1 = float(random.randint(1,10000))/10000
-                y2 = float(random.randint(1,10000))/10000
-                y3 = 1 - y1 - y2
-                if y3>0:
-                        break
 
-        return (y1,y2,y3)
 
 def generate_y():
-        y=[]
-        for i in range(q):
-                y_sample = generate_sample()
-                y.append( y_sample )
+        y = np.random.multivariate_normal(m, V, q)
         return y
 
 def F_beta(x,alpha):
-        ans = 0
-        ans+=alpha
+        ans = 0.0
+        ans += alpha
+        sum_loss = 0.0
+        y = generate_y()
         for i in range(q):
-                loss = 
+                loss = (-1) * x.dot(y[i]) - alpha
+                if loss > 0:
+                        sum_loss += loss
+        sum_loss/=(q*(1-beta))
+        ans+=sum_loss
+        return ans
+
+
+def F_minimize():
+        alpha = 0.06795
+        F_beta_min = 1
+        x1 =0.0
+        x2 = 0.0
+        x3 = 0.0
+        for a in range(10000):
+                for b in range(10000):
+                        x1 = float(a)/10000
+                        x2 = float(b)/10000
+                        if x1+x2>0.5:
+                                continue
+                        x3 = 1- x1 -x2
+                        f_b = F_beta(np.array([x1, x2, x3]), alpha)
+                        if f_b < F_beta_min:
+                                F_beta_min = f_b
+                                print F_beta_min,"x: ",x1,x2,x3
+        return F_beta_min
+
 time1 = time.time()
-y = generate_y()
+print F_beta(np.array([0.35250, 0.15382, 0.49368]), 0.06795)
 print time.time() - time1
 
         
