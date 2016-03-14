d1a28341f4c39ba44e586545e0f078d00054b211
analyzehim <raevegnforspam@rambler.ru>
2016-03-14 22:24:07 +0300

    Complete minimization. Make a global mu and sigma left

--- matlab/prog.m
+++ matlab/prog.m
@@ -1,14 +1,11 @@
 %fmincon(fun,x0,A,b,Aeq,beq,lb,ub)
 
-v = [0.35250 0.15382 0.49368 0.06795];
+% This is example v = [0.35250 0.15382 0.49368 0.06795];
 disp('_______________________________')
 disp('Begin')
-global cons;
-cons = 2;
-%[x,fval] = fmincon(@myfun,v,[0 0 0 0],0,[1 1 1 0],1, [0 0 0 0], [1 1 1 100]);
+[x,fval] = fmincon(@Fbeta,v,[0 0 0 0],0,[1 1 1 0],1, [0.0001 0.0001 0.0001 0.01], [1 1 1 100]);
 
-disp(Fbeta(v))
-%disp('f = ')
-%disp (fval)
-%disp ('x')
-%disp(x)
+disp('f = ')
+disp (fval)
+disp ('x')
+disp(x)
